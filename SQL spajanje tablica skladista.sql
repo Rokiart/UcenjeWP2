12e8c4896f81367bb3ab5eea89ab2d6dcaa76b4c
Roman <149431633+Rokiart@users.noreply.github.com>
2023-11-28 20:26:21 +0100

    spajanje tablica

---
+++ SQL spajanje tablica skladista.sql
@@ -0,0 +1,15 @@
+
+
+
+
+select a.sifra,a.brojizdatnice,a.datum,b.ime,b.prezime,c.ime,c.prezime
+,e.naziv,d.kolicina,
+concat(e.naziv,sifraproizvoda , e.mjernajedinica) as proizvod
+from izdatnice a inner join osobe b 
+on b.sifra=a. osoba
+left join skladistari c
+on c.sifra=a.skladistar
+inner join izdatniceproizvodi d
+on a.sifra=d.izdatnica
+inner join proizvodi e
+on d.proizvod=e.sifra;
